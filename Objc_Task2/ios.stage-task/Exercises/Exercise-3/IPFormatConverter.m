0a15a4da17b790011ea675fb6709f12351e78a8a
Sergey <80987789+SergeyGordeychik86@users.noreply.github.com>
2021-05-15 00:58:46 +0300

    Update IPFormatConverter.m

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,27 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    if (numbersArray == nil || numbersArray.count == 0) {
+        return @"";
+    }
+    NSString *numberNotInRange = @"The numbers in the input array can be in the range from 0 to 255.";
+    NSString *numberIsNegative = @"Negative numbers are not valid for input.";
+    NSMutableString *result = [[NSMutableString alloc] init];
+    for (NSInteger i = 0; i < 4; i++) {
+        NSInteger nextOctet = 0;
+        if (i < numbersArray.count) {
+            nextOctet = [numbersArray[i] intValue];
+        }
+        if (nextOctet > 255) {
+            return numberNotInRange;
+        } else if (nextOctet < 0) {
+            return numberIsNegative;
+        } else {
+            [result appendFormat:@"%ld.", nextOctet];
+        }
+    }
+    
+    return [result substringToIndex:[result length] - 1];
 }
 
 @end
